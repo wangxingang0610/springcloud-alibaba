3be2161238502a725e4fa980e261b95703b5e76c
wangxingang4 <wangxingang@jd.com>
2020-09-29 10:40:56 +0800

    微服务调用

--- shop-order/src/main/java/com/jd/controller/OrderController.java
+++ shop-order/src/main/java/com/jd/controller/OrderController.java
@@ -7,8 +7,8 @@ import com.jd.service.OrderService;
 //import com.jd.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.cloud.client.ServiceInstance;
-//import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +27,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-//    @Autowired
-//    private DiscoveryClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
 //    @Autowired
 //    private ProductService productService;
@@ -125,45 +125,20 @@ public class OrderController {
      * @param productId
      * @return
      */
-//    @GetMapping("/order/prod/{productId}")
-//    public Order order(@PathVariable("productId") Long productId) {
-//        log.info(">>客户下单，这时候要调用商品微服务查询商品信息");
-//
-//
-//        //从nacos中获取服务地址
-//        ServiceInstance serviceInstance = discoveryClient.getInstances("service-product").get(0);
-//        String url = serviceInstance.getHost() + ":" + serviceInstance.getPort();
-//
-//
-//
-//        //通过restTemplate调用商品微服务
-//        Product product = restTemplate.getForObject(
-//                "http://" + url + "/product/" + productId, Product.class);
-//
-//        log.info(">>商品信息,查询结果:" + JSON.toJSONString(product));
-//        Order order = new Order();
-//        order.setUserId(1L);
-//        order.setUsername("测试用户");
-//        order.setProductId(product.getId());
-//        order.setProductName(product.getName());
-//        order.setProductPrice(product.getPrice());
-//        order.setNumber(1);
-//        orderService.save(order);
-//        return order;
-//    }
-
-    /**
-     * http://localhost:8091/order/prod/1
-     * @param productId
-     * @return
-     */
     @GetMapping("/order/prod/{productId}")
     public Order order(@PathVariable("productId") Long productId) {
         log.info(">>客户下单，这时候要调用商品微服务查询商品信息");
 
+
+        //从nacos中获取服务地址
+        ServiceInstance serviceInstance = discoveryClient.getInstances("service-product").get(0);
+        String url = serviceInstance.getHost() + ":" + serviceInstance.getPort();
+
+
+
         //通过restTemplate调用商品微服务
         Product product = restTemplate.getForObject(
-                "http://localhost:8081/product/" + productId, Product.class);
+                "http://" + url + "/product/" + productId, Product.class);
 
         log.info(">>商品信息,查询结果:" + JSON.toJSONString(product));
         Order order = new Order();
@@ -176,6 +151,31 @@ public class OrderController {
         orderService.save(order);
         return order;
     }
+
+    /**
+     * http://localhost:8091/order/prod/1
+     * @param productId
+     * @return
+     */
+//    @GetMapping("/order/prod/{productId}")
+//    public Order order(@PathVariable("productId") Long productId) {
+//        log.info(">>客户下单，这时候要调用商品微服务查询商品信息");
+//
+//        //通过restTemplate调用商品微服务
+//        Product product = restTemplate.getForObject(
+//                "http://localhost:8081/product/" + productId, Product.class);
+//
+//        log.info(">>商品信息,查询结果:" + JSON.toJSONString(product));
+//        Order order = new Order();
+//        order.setUserId(1L);
+//        order.setUsername("测试用户");
+//        order.setProductId(product.getId());
+//        order.setProductName(product.getName());
+//        order.setProductPrice(product.getPrice());
+//        order.setNumber(1);
+//        orderService.save(order);
+//        return order;
+//    }
 
 
 }
